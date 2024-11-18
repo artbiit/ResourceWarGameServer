ff0885d8de28682607c3500451a4ae74ee99edaf
ljs <artbiit@naver.com>
2024-11-18 23:05:51 +0900

    fix 환경변수 보안성 및 값 저장 실수 처리

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Init/DotEnv.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Init/DotEnv.cs
@@ -22,9 +22,11 @@ namespace ResourceWar.Server
                 Logger.LogWarning($"[DotEnv] .env file not found at : {filePath}");
             }
 
+            int lineNumber = 0;
             foreach (var line in File.ReadAllLines(filePath))
             {
-                if(string.IsNullOrWhiteSpace(line) || line.StartsWith('#'))
+                lineNumber++;
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith('#'))
                 {
                     continue;
                 }
@@ -32,7 +34,7 @@ namespace ResourceWar.Server
                 var split = line.Split('=',2);
                 if (split.Length != 2)
                 {
-                    Logger.LogError($"[DotEnv] Syntax Error. length is over 2 : {line}");
+                    Logger.LogError($"[DotEnv] Syntax Error. line number : {lineNumber}");
                     continue;
                 }
 
@@ -53,13 +55,13 @@ namespace ResourceWar.Server
 
         private static void StoreValue(string key, string value)
         {
-            if(int.TryParse(key, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture, out var intValue))
+            if(int.TryParse(value, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture, out var intValue))
             {
                 Add(key, intValue);
-            }else if(float.TryParse(key, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out var floatValue))
+            }else if(float.TryParse(value, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out var floatValue))
             {
                 Add(key, floatValue);
-            }else if(bool.TryParse(key, out var boolValue))
+            }else if(bool.TryParse(value, out var boolValue))
             {
                 Add(key, boolValue);
             }
@@ -71,22 +73,21 @@ namespace ResourceWar.Server
 
        public static void Add<T>(string key, T value)
         {
-            Logger.Log($"{typeof(T)} Add => {key} / {value}");
             if (!envs.ContainsKey(typeof(T)))
             {
                 envs.Add(typeof(T), new Dictionary<string, object>());
             }
 
             envs[typeof(T)].Add(key, value);
-            
        }
 
-        public static T Get<T>(string key, T defaultValue = default(T))
+        public static T Get<T>(string key, T defaultValue = default)
         {
-            if(envs.TryGetValue(typeof(T), out var typeDictionary) && typeDictionary.TryGetValue(key, out var value))
+            if (envs.TryGetValue(typeof(T), out var typeDict) && typeDict.TryGetValue(key, out var value))
             {
                 return (T)value;
             }
+
             return defaultValue;
         }
     }
