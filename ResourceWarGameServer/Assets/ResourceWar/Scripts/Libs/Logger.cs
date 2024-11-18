0f52f5a8629417f8d937a1133e8d9e302633cb1e
ljs <artbiit@naver.com>
2024-11-18 16:07:53 +0900

    fix 사용 안하는 참조 제거

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Libs/Logger.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Libs/Logger.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using System.IO.Compression;
 
