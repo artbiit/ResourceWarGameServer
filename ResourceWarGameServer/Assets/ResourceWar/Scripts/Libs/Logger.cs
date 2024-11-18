7bf78eb90e95f4210b2923ad4caa7337f4820798
ljs <artbiit@naver.com>
2024-11-18 15:10:39 +0900

    fix 로거 스켈레톤 추가 및 어셈블리 정의 수정

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Libs/Logger.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Libs/Logger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,124 @@ namespace ResourceWar.Server.Lib
 {
     public static class Logger
     {
-     
+        // Log
+        public static void Log(object message)
+        {
+            UnityLog(message);
+        }
+
+        public static void Log(object message, UnityEngine.Object context)
+        {
+            UnityLog(message, context);
+        }
+
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        private static void UnityLog(object message, UnityEngine.Object context = null)
+        {
+            if (context == null)
+            {
+                UnityEngine.Debug.Log(message);
+            }
+            else
+            {
+                UnityEngine.Debug.Log(message, context);
+            }
+        }
+
+        // LogWarning
+        public static void LogWarning(object message)
+        {
+            UnityLogWarning(message);
+        }
+
+        public static void LogWarning(object message, UnityEngine.Object context)
+        {
+            UnityLogWarning(message, context);
+        }
+
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        private static void UnityLogWarning(object message, UnityEngine.Object context = null)
+        {
+            if (context == null)
+            {
+                UnityEngine.Debug.LogWarning(message);
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning(message, context);
+            }
+        }
+
+        // LogError
+        public static void LogError(object message)
+        {
+            UnityLogError(message);
+        }
+
+        public static void LogError(object message, UnityEngine.Object context)
+        {
+            UnityLogError(message, context);
+        }
+
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        private static void UnityLogError(object message, UnityEngine.Object context = null)
+        {
+            if (context == null)
+            {
+                UnityEngine.Debug.LogError(message);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(message, context);
+            }
+        }
+
+        // LogAssertion
+        public static void LogAssertion(object message)
+        {
+            UnityLogAssertion(message);
+        }
+
+        public static void LogAssertion(object message, UnityEngine.Object context)
+        {
+            UnityLogAssertion(message, context);
+        }
+
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        private static void UnityLogAssertion(object message, UnityEngine.Object context = null)
+        {
+            if (context == null)
+            {
+                UnityEngine.Debug.LogAssertion(message);
+            }
+            else
+            {
+                UnityEngine.Debug.LogAssertion(message, context);
+            }
+        }
+
+        // LogException
+        public static void LogException(Exception exception)
+        {
+            UnityLogException(exception);
+        }
+
+        public static void LogException(Exception exception, UnityEngine.Object context)
+        {
+            UnityLogException(exception, context);
+        }
+
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        private static void UnityLogException(Exception exception, UnityEngine.Object context = null)
+        {
+            if (context == null)
+            {
+                UnityEngine.Debug.LogException(exception);
+            }
+            else
+            {
+                UnityEngine.Debug.LogException(exception, context);
+            }
+        }
     }
 }
