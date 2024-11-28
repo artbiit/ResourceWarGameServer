8fcdad053c2bec97eea932e667856ae1c7b4e066
YongHyeon_Park <dydgustmdfl@gmail.com>
2024-11-28 18:09:30 +0900

    [update] [SyncRoomNoti 테스트 전까지 완료, 아직 필요한 모든 부분들을 추가하진 못했음 기능만 만들었음]
    게임별 분리:
    GameToken을 사용해 각 게임 서버의 Redis 데이터를 구분.
    플레이어 리스트(List 구조)와 개별 플레이어 데이터(Hash 구조)로 관리.
    
    효율적인 데이터 조회:
    ListRangeAsync로 플레이어 ID를 조회한 뒤, 필요한 데이터를 HashGetAllAsync로 가져옴.
    
    통합된 알림 처리:
    Redis 데이터를 기반으로 S2CSyncRoomNoti 메시지를 구성해 클라이언트에 전송
    
    Redis 동기화: Redis와 게임 상태를 동기화.
    플레이어 관리: 특정 게임 토큰 기준으로 플레이어를 추가, 삭제, 수정.
    데이터 전송: 특정 유저, 팀, 또는 전체 유저에게 데이터 전송.
    
    PostGreSQL: userData 저장하는 예제로 추가,
    
    현재 에로사항: Redis또는 PostGreSQL에서 UserName또는 avartarId를 가져와야하는데 어디서 가져와야할지 모르겠음

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Game/Player.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Game/Player.cs
@@ -1,16 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using StackExchange.Redis;
+using System.Linq;
 
 namespace ResourceWar.Server
 {
-    public class Player 
+    public class Player
     {
         public readonly int ClientId;
 
+        public string UserName { get; set; }
+        public bool IsReady { get; set; }
+        public bool Connected { get; set; }
+        public int LoadProgress { get; set; }
+        public int TeamId { get; set; }
+        public int AvatarId { get; set; }
+
         public Player(int clientId)
         {
-            this.ClientId = clientId;
+            ClientId = clientId;
+            IsReady = false;
+            Connected = true;
+            LoadProgress = 0;
+            TeamId = 0;
+        }
+
+        public static Player FromRedisData(int clientId, HashEntry[] redisValues)
+        {
+            return new Player(clientId)
+            {
+                UserName = (redisValues.First(x => x.Name == "user_name").Value).ToString(),
+                IsReady = bool.Parse(redisValues.First(x => x.Name == "is_ready").Value),
+                Connected = bool.Parse(redisValues.First(x => x.Name == "connected").Value),
+                LoadProgress = int.Parse(redisValues.First(x => x.Name == "load_progress").Value),
+                TeamId = int.Parse(redisValues.First(x => x.Name == "team_id").Value),
+                AvatarId = int.Parse(redisValues.First(x => x.Name == "avatar_id").Value)
+            };
+        }
+
+        public HashEntry[] ToRedisHashEntries()
+        {
+            return new HashEntry[]
+            {
+                new HashEntry("user_name", UserName.ToString()),
+                new HashEntry("is_ready", IsReady.ToString()),
+                new HashEntry("connected", Connected.ToString()),
+                new HashEntry("load_progress", LoadProgress.ToString()),
+                new HashEntry("team_id", TeamId.ToString()),
+                new HashEntry("avatar_id", AvatarId.ToString())
+            };
         }
     }
 }
--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/GameManager.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/GameManager.cs
@@ -6,9 +6,15 @@ using Cysharp.Threading.Tasks;
 using Logger = ResourceWar.Server.Lib.Logger;
 using System.Linq;
 using static UnityEditor.Experimental.GraphView.GraphView;
+using Protocol;
+using Google.Protobuf;
 
 namespace ResourceWar.Server
 {
+    /// <summary>
+    /// 게임 관리 클래스.
+    /// 게임 상태 관리 및 플레이어 등록, 데이터 전송 등의 주요 기능을 담당.
+    /// </summary>
     public class GameManager : MonoBehaviour
     {
         public enum State : int
@@ -20,6 +26,7 @@ namespace ResourceWar.Server
             PLAYING
         }
 
+        // GameManager에서 처리하느 주요 이벤트를 정의하는 열거형
         public enum GameManagerEvent
         {
             SendPacketForAll = 0,
@@ -28,14 +35,18 @@ namespace ResourceWar.Server
             AddNewPlayer = 3,
         }
 
+        // 현재 게임 상태를 저장
         public State GameState { get; private set; } = State.CREATING;
+        // 게임의 고유 토큰 (서버가 게임 세션을 식별하기 위해 사용)
         public string GameToken { get; private set; }
+        // 이벤트 구독 여부를 확인하는 플래그
         private bool subscribed = false;
 
         /// <summary>
         /// 0 - Gray(팀 선택x), 1 - Blue, 2 - Red
         /// </summary>
         private Team[] teams = null;
+        // 현재 등록된 플레이어 수
         private int playerCount = 0;
 
         private void Awake()
@@ -55,52 +66,121 @@ namespace ResourceWar.Server
             await SetState(State.LOBBY);
         }
 
+        /// <summary>
+        /// 게임 상태 변경 및 Redis 서버에 상태 저장
+        /// </summary>
+        /// <param name="state">변경할 게임 상태</param>
+        /// <returns></returns>
         public async UniTask SetState(State state)
         {
             this.GameState = state;
             await GameRedis.SetGameState(state);
         }
 
+        /// <summary>
+        /// 이벤트에 대한 구독 설정
+        /// </summary>
         private void Subscribes()
         {
+            // 재구독 방지
             if (subscribed)
             {
                 return;
             }
             subscribed = true;
             
+            // 패킷 전송 관련 이벤트 등록
             var sendDispatcher = EventDispatcher<GameManagerEvent, Packet>.Instance;
             sendDispatcher.Subscribe(GameManagerEvent.SendPacketForAll, SendPacketForAll);
             sendDispatcher.Subscribe(GameManagerEvent.SendPacketForTeam, SendPacketForTeam);
             sendDispatcher.Subscribe(GameManagerEvent.SendPacketForUser, SendPacketForUser);
              
+            // 플레이어 등록 관련이벤트 등록
             var receivedDispatcher  = EventDispatcher<GameManagerEvent, ReceivedPacket>.Instance;
             receivedDispatcher.Subscribe(GameManagerEvent.AddNewPlayer, RegisterPlayer);
 
 
         }
 
-        public UniTask RegisterPlayer(ReceivedPacket receivedPacket)
+        /// <summary>
+        /// 새로운 플레이어 등록
+        /// </summary>
+        /// <param name="receivedPacket">등록 요청 데이터를 포함한 패킷</param>
+        /// <returns></returns>
+        public async UniTask RegisterPlayer(ReceivedPacket receivedPacket)
         {
-            if(playerCount >= 4)
+            if (playerCount >= 4)
             {
-                return UniTask.FromException(new System.InvalidOperationException("Player count has reached its maximum limit."));
+                throw new System.InvalidOperationException("Player count has reached its maximum limit.");
             }
+
             var token = receivedPacket.Token;
             var clientId = receivedPacket.ClientId;
 
-            if(teams.Any(t => t.ContainsPlayer(token)))
+            if (teams.Any(t => t.ContainsPlayer(token)))
             {
-               return UniTask.FromException(new System.InvalidOperationException($"Already exsits player[{clientId}] : {token}"));
+                throw new System.InvalidOperationException($"Already exists player[{clientId}] : {token}");
             }
+
+            // 플레이어 객체 생성 및 팀 0에 추가
             var player = new Player(clientId);
             teams[0].Players.Add(token, player);
             playerCount++;
             Logger.Log($"Add New Player[{clientId}] : {token}");
-            return UniTask.CompletedTask;
+
+            var userName = await PlayerRedis.GetUserName(GameToken, clientId);
+
+            // Redis에 플레이어 정보 저장
+            // AratarI는 어딘가에서 가져와야하는데 아직 모름
+            await PlayerRedis.AddPlayerInfo(
+                gameToken: GameToken,
+                clientId: clientId,
+                userName: userName,
+                isReady: false, // Default values
+                connected: true,
+                loadProgress: 0,
+                teamId: 0,
+                avatarId: 1
+            );
+
+            // Notify all players in the same lobby
+            await NotifyRoomState();
         }
 
+        public async UniTask NotifyRoomState()
+        {
+            var players = await PlayerRedis.GetAllPlayersInfo(GameToken);
 
+            var syncRoomNoti = new S2CSyncRoomNoti
+            {
+                Players =
+                {
+                    players.Select(p => new PlayerRoomInfo
+                    {
+                        PlayerId = (uint)p.ClientId,
+                        PlayerName = p.UserName,
+                        AvartarItem = (uint)p.AvatarId,
+                        TeamIndex = (uint)p.TeamId,
+                        Ready = p.IsReady,
+                    })
+                }
+            };
+
+            var packet = new Packet
+            {
+                PacketType = PacketType.SYNC_ROOM_NOTIFICATION,
+                Token = "",
+                Payload = syncRoomNoti
+            };
+
+            await SendPacketForAll(packet);
+        }
+
+        /// <summary>
+        /// 플레이어 검색
+        /// </summary>
+        /// <param name="token">플레이어의 고유 토큰</param>
+        /// <returns>찾은 플레이어 객체 or Null</returns>
         public Player FindPlayer(string token)
         {
             foreach (var team in teams)
