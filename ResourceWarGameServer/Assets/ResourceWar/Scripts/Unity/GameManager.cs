92263b625eb6f25d92f72699564187a7b482f2fa
ljs <artbiit@naver.com>
2024-11-28 12:26:26 +0900

    fix 빌드에 문제되는 부분 제거

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/GameManager.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/GameManager.cs
@@ -5,7 +5,6 @@ using ResourceWar.Server.Lib;
 using Cysharp.Threading.Tasks;
 using Logger = ResourceWar.Server.Lib.Logger;
 using System.Linq;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace ResourceWar.Server
 {
