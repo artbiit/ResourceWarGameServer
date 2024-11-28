61a91d5132bf893f71f621a1d2a9f172c02a6565
ljs <artbiit@naver.com>
2024-11-28 17:16:32 +0900

    fix 초기화 방식 수정중

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Net/Data/Enums.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Net/Data/Enums.cs
@@ -17,6 +17,7 @@ namespace ResourceWar.Server
         DESTROY,
         LOBBY,
         LOADING,
-        PLAYING
+        PLAYING,
+        ERROR = 1,
     }
 }
--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/Initializer.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/Initializer.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using ResourceWar.Server.Lib;
 using Logger = ResourceWar.Server.Lib.Logger;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Linq;
+using Cysharp.Threading.Tasks.Linq;
+
 
 namespace ResourceWar.Server
 {
@@ -12,16 +17,65 @@ namespace ResourceWar.Server
 
         private void Awake()
         {
+            Init().Forget();
+        }
+
+        private async UniTask Init()
+        {
             Logger.Log("-------------Initializer-------------");
+            await GameRedis.SetGameState(ServerState.CREATING);
             Logger.Log("Start DataLayer");
             DotEnv.Config();
-            PostgreSQLClient.Instance.Connect(DotEnv.Get<string>("DB_HOST"), DotEnv.Get<int>("DB_PORT"), DotEnv.Get<string>("DB_NAME"), DotEnv.Get<string>("DB_USER"), DotEnv.Get<string>("DB_PASSWORD"), DotEnv.Get<int>("DB_CONNECTION_LIMIT_MIN"), DotEnv.Get<int>("DB_CONNECTION_LIMIT_MAX"));
-            RedisClient.Instance.Connect(DotEnv.Get<string>("REDIS_HOST"), DotEnv.Get<int>("REDIS_PORT"), DotEnv.Get<string>("REDIS_PASSWORD"));
-            Logger.Log("-------------DataLayer Initialized-------------");
-            TcpServer.Instance.Init(DotEnv.Get<string>("SERVER_BIND"), DotEnv.Get<int>("SERVER_PORT"));
-            Logger.Log("-------------Initializer-------------");
+            var (postgresqlResult, redisResult) = await UniTask.WhenAll(TryInitialize(PostogresqlInit), TryInitialize(RedisInit));
+            if(postgresqlResult && redisResult)
+            {
+                Logger.Log("-------------DataLayer Initialized-------------");
+                TcpServer.Instance.Init(DotEnv.Get<string>("SERVER_BIND"), DotEnv.Get<int>("SERVER_PORT"));
+                Logger.Log("-------------Initializer-------------");
+                await GameRedis.SetGameState(ServerState.LOBBY);
+            }
+            else
+            {
+                Logger.LogError("-------------DataLayer Initialize Failed-------------");
+                await GameRedis.SetGameState(ServerState.ERROR);
+#if UNITY_EDITOR
+                Application.Quit(1);
+#endif
+            }
+            
+            
             //SceneManager.LoadScene("Game");
         }
-       
+
+        private async UniTask<bool> PostogresqlInit() => await PostgreSQLClient.Instance.ConnectAsync(DotEnv.Get<string>("DB_HOST"), DotEnv.Get<int>("DB_PORT"), DotEnv.Get<string>("DB_NAME"), DotEnv.Get<string>("DB_USER"), DotEnv.Get<string>("DB_PASSWORD"), DotEnv.Get<int>("DB_CONNECTION_LIMIT_MIN"), DotEnv.Get<int>("DB_CONNECTION_LIMIT_MAX"));
+        private async UniTask<bool> RedisInit() => await RedisClient.Instance.ConnectAsync(DotEnv.Get<string>("REDIS_HOST"), DotEnv.Get<int>("REDIS_PORT"), DotEnv.Get<string>("REDIS_PASSWORD"));
+
+
+        private async UniTask<bool> TryInitialize(Func<UniTask<bool>> tryFunction, int tryCount = 5)
+        {
+            bool result = false;
+            for (int i = 0; i < tryCount; i++)
+            {
+                try
+                {
+                    result = await tryFunction();
+                    if (result)
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex);
+                }
+
+                if (i < tryCount - 1) 
+                {
+                    await UniTask.Delay((i + 1) * 1000);
+                }
+            }
+
+            return result;
+        }
     }
 }
