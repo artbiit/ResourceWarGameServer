3a1a9dd132561c0508bd4b35bcda4e1e0da4f85a
ljs <artbiit@naver.com>
2024-12-02 15:17:09 +0900

    fix  최신화

--- ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/States/Monster/Idle.cs
+++ ResourceWarGameServer/Assets/ResourceWar/Scripts/Unity/States/Monster/Idle.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using ResourceWar.Server;
 using Cysharp.Threading.Tasks;
-
+using ResourceWar.Server.Lib;
 namespace ResourceWar.Server.Monster
 {
-    public class Idle : IAsyncState<MonsterStats>
+    public class Idle : IAsyncState<MonsterBehaviour>
     {
-        public async UniTask Enter(MonsterStats stats)
+        public async UniTask Enter(MonsterBehaviour monster)
         {
             await UniTask.Yield();
         }
 
-        public async UniTask Execute(MonsterStats stats)
+        public async UniTask Execute(MonsterBehaviour monster)
         {
             await UniTask.Yield();
         }
 
-        public async UniTask Exit(MonsterStats stats)
+        public async UniTask Exit(MonsterBehaviour monster)
         {
             await UniTask.Yield();
         }
